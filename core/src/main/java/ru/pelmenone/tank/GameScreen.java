647f10c57b55215d3e071c4908236d906d6d90a5
dmitrii <pelmenone1@gmail.com>
2025-05-23 17:54:58 +0600

    shooting(up, down, left, right) (mobile shooting)

--- core/src/main/java/ru/pelmenone/tank/GameScreen.java
+++ core/src/main/java/ru/pelmenone/tank/GameScreen.java
@@ -20,6 +20,8 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 
@@ -85,6 +87,10 @@ public class GameScreen implements Screen {
     private boolean gameWon;
     private BitmapFont font;
 
+    //время
+    private float timer = 0;
+    private final float INTERVAL = 2.0f;
+
 
     public GameScreen(final Main game) {
         this.game = game;
@@ -111,14 +117,10 @@ public class GameScreen implements Screen {
         playerTank.y = 100;
         playerTank.width = 40;
         playerTank.height = 40;
-        if (Gdx.app.getType() == Application.ApplicationType.Android) {
-            // Специфичные настройки для Android
-            float uiScale = 1.5f; // Увеличиваем UI на мобильных
-        }
 
         // Инициализация врагов
         enemies = new Array<>();
-        spawnEnemies(2);
+        spawnEnemies(MathUtils.random(1, 10));
 
         // Инициализация стен
         walls = new Array<>();
@@ -167,10 +169,12 @@ public class GameScreen implements Screen {
 
 
     private void spawnEnemies(int count) {
+        float counter;
+
         for (int i = 0; i < count; i++) {
             Rectangle enemy = new Rectangle();
-            enemy.x = 600 + i * 100;
-            enemy.y = 100 + i * 100;
+            enemy.x = MathUtils.random(250, 500) + i * 100;
+            enemy.y = MathUtils.random(100, 400) + i * 100;
             enemy.width = 40;
             enemy.height = 40;
             enemies.add(enemy);
@@ -248,6 +252,12 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        timer += delta; // Увеличиваем таймер на время, прошедшее с прошлого кадра
+
+        if (timer >= INTERVAL) {
+            timer = 0; // Сбрасываем таймер
+            shoot();
+        }
         // Очистка экрана
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -260,17 +270,6 @@ public class GameScreen implements Screen {
 
         // Отрисовка
         game.batch.begin();
-        font.draw(game.batch, "enemies: " + enemiesRemaining, 20, 450);
-        // кнопка fire
-        Texture fireTex = isFireButtonPressed ? fireButtonPressedTexture : fireButtonTexture;
-        game.batch.draw(
-            fireTex,
-            fireButton.x,
-            fireButton.y,
-            fireButton.width,
-            fireButton.height
-        );
-        font.draw(game.batch, "FIRE", fireButton.x + fireButton.width/3, fireButton.y + fireButton.height/2);
 
         // Фон
         game.batch.draw(backgroundTexture, 0, 0, 800, 480);
@@ -279,10 +278,6 @@ public class GameScreen implements Screen {
         for (Rectangle wall : walls) {
             game.batch.draw(wallTexture, wall.x, wall.y, wall.width, wall.height);
         }
-        for (Rectangle kust : kusts) {
-            game.batch.draw(kustTexture, kust.x, kust.y, kust.width, kust.height);
-        }
-
         // Игрок
         float textureBaseRotation = 90;;
         game.batch.draw(tankTexture,
@@ -299,14 +294,19 @@ public class GameScreen implements Screen {
         for (Rectangle enemy : enemies) {
             game.batch.draw(enemyTexture, enemy.x, enemy.y, enemy.width, enemy.height);
         }
-
         // Пули
         for (Bullet bullet : bullets) {
             game.batch.draw(bulletTexture, bullet.rect.x, bullet.rect.y, bullet.rect.width, bullet.rect.height);
         }
+        // KUSTS
+        for (Rectangle kust : kusts) {
+            game.batch.draw(kustTexture, kust.x, kust.y, kust.width, kust.height);
+        }
+        font.draw(game.batch, "enemies: " + enemiesRemaining, 10, 480);
+        font.draw(game.batch, "FIRE", 650, 70);
 
         if (gameWon) {
-            //font.draw(game.batch, "WIN", camera.viewportWidth/2 - 30, camera.viewportHeight/2);
+            font.draw(game.batch, "WIN", camera.viewportWidth/2 - 30, camera.viewportHeight/2);
 
             font.draw(game.batch, "AGAIN",
                 camera.viewportWidth/2 - 70,
@@ -393,7 +393,7 @@ public class GameScreen implements Screen {
     public void restartGame() {
         enemies.clear();
         bullets.clear();
-        spawnEnemies(5);
+        spawnEnemies(MathUtils.random(1, 10)   );
         enemiesRemaining = enemies.size;
         gameWon = false;
 
@@ -403,31 +403,31 @@ public class GameScreen implements Screen {
 
     private void handleInput(float delta) {
         boolean moved = false;
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP) || Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
             lastPressedDirection = MovementDirection.UP;
             tankRotation = 90; // Поворот вверх (0 градусов)
             moved = true;
         }
-        else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN) || Gdx.input.isKeyJustPressed(Input.Keys.S)) {
+        else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
             lastPressedDirection = MovementDirection.DOWN;
             tankRotation = 270; // Поворот вверх (0 градусов)
             moved = true;
         }
-        else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT) || Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+        else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
             lastPressedDirection = MovementDirection.LEFT;
             tankRotation = 180; // Поворот вверх (0 градусов)
             moved = true;
         }
-        else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT) || Gdx.input.isKeyJustPressed(Input.Keys.D)) {
+        else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
             lastPressedDirection = MovementDirection.RIGHT;
             tankRotation = 0; // Поворот вверх (0 градусов)
             moved = true;
         }
 
-        boolean upPressed = Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W);
-        boolean downPressed = Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S);
-        boolean leftPressed = Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A);
-        boolean rightPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D);
+        boolean upPressed = Gdx.input.isKeyPressed(Input.Keys.UP);
+        boolean downPressed = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+        boolean leftPressed = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+        boolean rightPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 
         if (!upPressed && !downPressed && !leftPressed && !rightPressed) {
             currentDirection = MovementDirection.NONE;
@@ -435,9 +435,9 @@ public class GameScreen implements Screen {
         else if ((upPressed ? 1 : 0) + (downPressed ? 1 : 0) +
             (leftPressed ? 1 : 0) + (rightPressed ? 1 : 0) == 1) {
             if (upPressed) currentDirection = MovementDirection.UP;
-            else if (downPressed) currentDirection = MovementDirection.DOWN;
-            else if (leftPressed) currentDirection = MovementDirection.LEFT;
-            else if (rightPressed) currentDirection = MovementDirection.RIGHT;
+            if (downPressed) currentDirection = MovementDirection.DOWN;
+            if (leftPressed) currentDirection = MovementDirection.LEFT;
+            if (rightPressed) currentDirection = MovementDirection.RIGHT;
         }
         else {
             currentDirection = lastPressedDirection;
@@ -497,17 +497,29 @@ public class GameScreen implements Screen {
     private void shoot() {
         Bullet bullet = new Bullet();
         bullet.rect = new Rectangle();
-        bullet.rect.x = playerTank.x + playerTank.width/2;
-        bullet.rect.y = playerTank.y + playerTank.height/2;
+        bullet.rect.x = playerTank.x + playerTank.width/2 - 8;
+        bullet.rect.y = playerTank.y + playerTank.height/2 - 9;
 
-        bullet.rect.width = 20;
-        bullet.rect.height = 20;
-        bullet.direction = new Vector2(1, 0);
+        bullet.rect.width = 15;
+        bullet.rect.height = 15;
         float angleRad = (float)Math.toRadians(tankRotation);
 
         // Направление выстрела (единичный вектор)
-        bullet.directionX = (float)Math.sin(angleRad);
-        bullet.directionY = (float)Math.cos(angleRad);
+        if (lastPressedDirection == MovementDirection.RIGHT) {
+            bullet.direction = new Vector2(1, 0);
+        }
+        if (lastPressedDirection == MovementDirection.UP) {
+            bullet.direction = new Vector2(0, 1);
+        }
+        if (lastPressedDirection == MovementDirection.LEFT) {
+            bullet.direction = new Vector2(-1, 0);
+        }
+        if (lastPressedDirection == MovementDirection.DOWN) {
+            bullet.direction = new Vector2(0, -1);
+        }
+        if (lastPressedDirection == MovementDirection.NONE) {
+            bullet.direction = new Vector2(1, 0);
+        }
         bullet.speed = 500f;
         bullets.add(bullet);
         // звук выстрела
@@ -616,7 +628,14 @@ public class GameScreen implements Screen {
         // Проверка столкновений игрока со стенами
         for (Rectangle wall : walls) {
             if (playerTank.overlaps(wall)) {
-                // Простое "отталкивание" от стен
+                if (moveUp) playerTank.y -= 1;
+                if (moveDown) playerTank.y += 1;
+                if (moveLeft) playerTank.x += 1;
+                if (moveRight) playerTank.x -= 1;
+            }
+        }
+        for (Rectangle enemies: enemies){
+            if (playerTank.overlaps(enemies)) {
                 if (moveUp) playerTank.y -= 1;
                 if (moveDown) playerTank.y += 1;
                 if (moveLeft) playerTank.x += 1;
